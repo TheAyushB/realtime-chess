f7aaf0d40ccd34565c3ea55b89302cd5e5a45405
TheAyushB <ayushbhardwaj510@gmail.com>
2024-05-07 23:18:07 +0530

    added hook

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
 import { Button } from "../components/Button"
 import { ChessBoard } from "../components/ChessBoard"
+import { useSocket } from "../hooks/useSocket"
+import { Chess } from "chess.js";
 
+//TODO: move together, there's code repetition here
+export const INIT_GAME = "init_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
 
 export const Game = () => {
+
+    const socket = useSocket();
+    const [board, setBoard] = useState(new Chess());
+
+    useEffect(() => {
+        if (!socket) {
+            return;
+        }
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            console.log(message);
+            switch(message.type) {
+                case INIT_GAME:
+                    setBoard(new Chess());
+                    console.log("Game initialized");
+                    break;
+                case MOVE:
+                    console.log("Mode made");
+                    break;
+                case GAME_OVER:
+                    console.log("Game over");
+                    break;               
+                    
+            }
+        }
+    })
+
+    if(!socket) return <div>Connecting...</div>
     return <div className="justify-center flex">
         <div className="pt-8 max-w-screen-lg w-full">
             <div className="grid grid-cols-6 gap-4 w-full">
@@ -11,7 +46,9 @@ export const Game = () => {
                 </div>
                 <div className="col-span-2 bg-green-200 w-full">
                     <Button onClick={() => {
-                        navigate("/game")
+                        socket.send(JSON.stringify({
+                            type: INIT_GAME
+                        }))
                     }}>Play</Button>
                 </div>
             </div>
